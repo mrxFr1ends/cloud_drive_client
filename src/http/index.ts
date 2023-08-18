5a45aab2fb61a560e50b2d7a25cf94049ce70c89
mrx_fr1ends <prosto.pasha123@gmail.com>
2023-08-18 10:48:17 +0300

    Prepare to deploy, remove usefull libs and change style in Auth page

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:5000/api";
+export const API_URL = "https://cloud-drive-w6h8.onrender.com/api";
 
 export const $api = axios.create({
   baseURL: API_URL,
